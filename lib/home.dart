eae67dc18baf67fbba81cb538b591a4ee656c70c
Vinay Tiparadi <vinaytiparadi123@gmail.com>
2022-02-08 17:26:40 +0530

    use Stack for text upon card and design card1

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'card1.dart';
+
 // 1
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,8 +12,7 @@ class _HomeState extends State<Home> {
   int _selectedIndex = 0;
   // 8
   static List<Widget> pages = <Widget>[
-    // TODO: Replace with Card1
-    Container(color: Colors.red),
+    const Card1(),
     // TODO: Replace with Card2
     Container(color: Colors.green),
     // TODO: Replace with Card3

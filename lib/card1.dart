7567150b6d4a24813ca438b68075d84e61dfc5c4
Vinay Tiparadi <vinaytiparadi123@gmail.com>
2022-02-08 15:48:49 +0530

    create card1 widget

---
+++ lib/card1.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Card1 extends StatelessWidget {
+  const Card1({Key? key}) : super(key: key);
+  // 1
+  final String category = 'Editor\'s Choice';
+  final String title = 'The Art of Dough';
+  final String description = 'Learn to make the perfect bread.';
+  final String chef = 'Vinay Tiparadi';
+  // 2
+  @override
+  Widget build(BuildContext context) {
+    // 3
+    return Center(
+      // TODO: Card1 Decorate Container
+      child: Container(),
+    );
+  }
+}

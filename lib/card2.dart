25acfd8343eda56f0c909479898e0f9f203a7a34
Vinay Tiparadi <vinaytiparadi123@gmail.com>
2022-02-09 20:22:13 +0530

    compose author widget

--- lib/card2.dart
+++ lib/card2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'author_card.dart';
 
 class Card2 extends StatelessWidget {
   const Card2({Key? key}) : super(key: key);
@@ -23,7 +24,11 @@ class Card2 extends StatelessWidget {
         // 2
         child: Column(
           children: [
-            // TODO 1: add author information
+            const AuthorCard(
+              authorName: 'Mike Katz',
+              title: 'Smoothie Connoisseur',
+              imageProvider: AssetImage('assets/author_katz.jpeg'),
+            ),
             // TODO 4: add Positioned text
           ],
         ),

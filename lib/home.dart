e41a43141f99a187ecd43ad1f2ecbbe6bd0733e0
Vinay Tiparadi <vinaytiparadi123@gmail.com>
2022-02-10 15:40:39 +0530

    finish card3

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'card1.dart';
 import 'card2.dart';
+import 'card3.dart';
 
 // 1
 class Home extends StatefulWidget {
@@ -15,8 +16,7 @@ class _HomeState extends State<Home> {
   static List<Widget> pages = <Widget>[
     const Card1(),
     const Card2(),
-    // TODO: Replace with Card3
-    Container(color: Colors.blue)
+    const Card3(),
   ];
   // 9
   void _onItemTapped(int index) {
